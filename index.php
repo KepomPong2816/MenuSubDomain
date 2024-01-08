8f48fb58750126a7ec47659f39fff295aae55ee3
KepomPong2816 <indratama2816@gmail.com>
2024-01-08 16:30:06 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Menu Application Project 2</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <title>Menu Application Project 2</title>
+</head>
+
+<body>
+  <!--Hey! This is the original version
+of Simple CSS Waves-->
+
+  <div class="header">
+
+    <!--Content before waves-->
+    <div class="inner-header flex">
+
+      <div class="menu">
+        <div class='sub-menu'>
+          <a href="http://gohit.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://liveit.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://ruanggambar.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://fitnessia.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://komaha.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://oncourse.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://rentcar.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
+          </a>
+        </div>
+
+        <div class='sub-menu'>
+          <a href="http://catcare.praktisimengajar.my.id">
+            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!--Waves Container-->
+    <div>
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+          <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+          <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+          <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+          <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+      </svg>
+    </div>
+    <!--Waves end-->
+
+  </div>
+  <!--Header ends-->
+
+  <!--Content starts-->
+  <div class="content flex">
+    <p>By Oki Bilal Indratama </p>
+  </div>
+  <!--Content ends-->
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
