f55b1c033b6d1a13bc1c07f759ded5073a54e0ac
Oki Bilal Indratama <116704301+KepomPong2816@users.noreply.github.com>
2024-01-22 16:32:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,96 +17,91 @@
   <!--Hey! This is the original version
 of Simple CSS Waves-->
 
-  <div class="header">  
+  <!--Hey! This is the original version
+of Simple CSS Waves-->
 
-    <!--Content before waves-->
-    <div class="inner-header flex">
+<div class="header">
 
-      <div class="menu">
-        <h1 style="margin-bottom: 30px;">Application Project 2</h1>
-        <div class='sub-menu'>
-          <a href="http://gohit.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
-            <div class="text">Gohit</div>
-          </a>
-        </div>
+<!--Content before waves-->
+<div class="inner-header flex">
 
-        <div class='sub-menu'>
-          <a href="http://liveit.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
-            <div class="text">Liveit</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://ruanggambar.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
-            <div class="text">Ruang Gambar</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://fitnessia.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
-            <div class="text">Fitnessia</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://komaha.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
-            <div class="text">Komaha</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://oncourse.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
-            <div class="text">On Course</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://rentcar.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu1">
-            <div class="text">Rent Car</div>
-          </a>
-        </div>
-
-        <div class='sub-menu'>
-          <a href="http://catcare.praktisimengajar.my.id">
-            <img src="https://www.pinclipart.com/picdir/middle/576-5766468_pig-avatar-ukuran-100-x-100-pixel-clipart.png" class="rounded-pill" id="imgmenu2">
-            <div class="text">Cat Care</div>
-          </a>
-        </div>
-      </div>
+  <div class="menu">
+    <h1 style="margin-bottom: 30px;">Application Project 2</h1>
+    <div class='sub-menu'>
+      <a href="http://gohit.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432431474036877/Gohit-1-15-2024.png?ex=65b79b88&is=65a52688&hm=8f712d04be37353bf2323a6e35c9682d32b442a59d448b0cdffa656f677cd8e5&=&format=webp&quality=lossless&width=1280&height=568" id="imgmenu1">
+      </a>
     </div>
-
-    <!--Waves Container-->
-    <div sty>
-      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-        <defs>
-          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g class="parallax">
-          <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-          <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-          <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-          <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
-        </g>
-      </svg>
+    
+    <div class='sub-menu'>
+      <a href="http://liveit.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432429599170581/Liveit-1-15-2024.png?ex=65b79b87&is=65a52687&hm=5cb37bf60e6164efbab16cff49ce0fde56d1fd6c3821ea82bcba2a98d256815f&=&format=webp&quality=lossless&width=1280&height=577" id="imgmenu2">
+      </a>
     </div>
-    <!--Waves end-->
-
+    
+    <div class='sub-menu'>
+      <a href="http://ruanggambar.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432429926322257/Ruang-Gambar-1-15-2024.png?ex=65b79b87&is=65a52687&hm=35d760bd46fa2831860b3945b5c775668bfd34ea6fbcc22e601a167ad41df983&=&format=webp&quality=lossless&width=1280&height=322" class="rounded-pill" id="imgmenu1">
+      </a>
+    </div>
+    
+    <div class='sub-menu'>
+      <a href="http://fitnessia.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432429284610098/Fitnessia-1-15-2024.png?ex=65b79b87&is=65a52687&hm=85b7108db3db96868207e3c135147db419e15fbed1bbed9f75758c7d89b5dd81&=&format=webp&quality=lossless&width=1280&height=417" class="rounded-pill" id="imgmenu2">
+      </a>
+    </div>
+    
+    <div class='sub-menu'>
+      <a href="http://komaha.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432430874239077/Komaha-1-15-2024.png?ex=65b79b88&is=65a52688&hm=96b4349a38e4e793f68eea4cc7ad263fd43389711085bd6bc11ce9dab8fa9336&=&format=webp&quality=lossless&width=1280&height=431" class="rounded-pill" id="imgmenu1">
+      </a>
+    </div>
+    
+    <div class='sub-menu'>
+      <a href="http://oncourse.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432431193002014/On-Course-1-15-2024.png?ex=65b79b88&is=65a52688&hm=495a909336e7874b7e0710dff47ae96ac0f408e90ca12ecc47f91b5feb1c4e8b&=&format=webp&quality=lossless&width=1280&height=373" class="rounded-pill" id="imgmenu2">
+      </a>
+    </div>
+    
+    <div class='sub-menu'>
+      <a href="http://rentcar.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432430580633700/Cat-Care-1-15-2024.png?ex=65b79b88&is=65a52688&hm=d2893bec6647a107095e5051a0be051b261c77f9e782e7a1d82cf860b4e14ca4&=&format=webp&quality=lossless&width=1280&height=412" class="rounded-pill" id="imgmenu1">
+      </a>
+    </div>
+    
+    <div class='sub-menu'>
+      <a href="http://catcare.praktisimengajar.my.id">
+        <img src="https://media.discordapp.net/attachments/549919551396249611/1196432430580633700/Cat-Care-1-15-2024.png?ex=65b79b88&is=65a52688&hm=d2893bec6647a107095e5051a0be051b261c77f9e782e7a1d82cf860b4e14ca4&=&format=webp&quality=lossless&width=1280&height=412" class="rounded-pill" id="imgmenu2">
+      </a>
+    </div>
   </div>
-  <!--Header ends-->
+</div>
 
-  <!--Content starts-->
-  <div class="content flex">
-    <p>By Oki Bilal Indratama </p>
-  </div>
-  <!--Content ends-->
+<!--Waves Container-->
+<div>
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g class="parallax">
+<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+</g>
+</svg>
+</div>
+<!--Waves end-->
 
+</div>
+<!--Header ends-->
+
+<!--Content starts-->
+<div class="content flex">
+  <p>By Oki Bilal Indratama </p>
+</div>
+<!--Content ends-->
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
